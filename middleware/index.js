fb0fad078aa5ea9b56da120cf6ede922efcb9695
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2018-06-13 18:24:43 +0530

    initial deploy commit

---
+++ middleware/index.js
@@ -0,0 +1,59 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// all the middleare goes here
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+            req.flash("error", "Campground not found");
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have the Permission to do that! ");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You don't have the Permission ! ");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+            req.flash("error", "Comment not found");
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have the Permission to do that! ");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // Adding flash messages
+    req.flash("error", "Please login to create New Campground!");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
